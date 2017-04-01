a3cb1ce9d04052e14655172ac730a07bf36a8a12
blha303 <stevensmith.ome@gmail.com>
2017-04-02 02:01:06 +0800

    Actually functioning somewhat

--- att.sh
+++ att.sh
@@ -1,15 +1,50 @@
 #!/bin/bash
-function block {
-	./att.py $1
+
+alias quit=exit
+
+STATE=0
+
+function block() {
+	python3 att.py $1
 	echo
-	sleep 3
+	if [ "$2" ]; then
+		sleep $2
+	fi
+}
+
+function prompt() {
+	echo -n "> "
+	read response
+	eval $response
+}
+
+function look() {
+	if [ "$1" == "picture" ]; then
+		echo "You look at the PICTURE OF THE MOON. It is a picture of the moon."
+	fi
+}
+
+function start() {
+	if [ "$1" == "conveyor" ]; then
+		if [ $STATE == 0 ]; then
+			block conveyor1 1
+			block conveyor2 1
+			block conveyor3 1
+			STATE=1
+			
+			block conveyor_start
+		else
+			block conveyor_running
+		fi
+	fi
 }
 
 clear
-block intro
+block intro 3
 clear
-block g1
-block g2
-block g3
+block g1 3
+block g2 3
+block g3 3
 block g4
+while true; do prompt; done
 
